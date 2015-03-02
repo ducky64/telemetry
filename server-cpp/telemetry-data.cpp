3ad81d13770c7e5d3bd7c115fb45a097c5c5ec63
ducky64 <elpato25@gmail.com>
2015-03-02 03:30:17 -0800

    templating funfunfun

--- server-cpp/telemetry-data.cpp
+++ server-cpp/telemetry-data.cpp
@@ -32,19 +32,6 @@ void Data::write_header_kvrs(TransmitPacketInterface& packet) {
   packet_write_string(packet, units);
 }
 
-template<typename T>
-size_t IntData<T>::get_header_kvrs_length() {
-  return Data::get_header_kvrs_length()
-      + 1 + 1;  // data length
-}
-
-template<typename T>
-void IntData<T>::write_header_kvrs(TransmitPacketInterface& packet) {
-  Data::write_header_kvrs(packet);
-  packet.write_uint8(RECORDID_INT_LENGTH);
-  packet.write_uint8(get_payload_length());
-}
-
 template<>
 size_t IntData<uint8_t>::get_payload_length() {
   return 1;
@@ -70,6 +57,21 @@ size_t IntData<uint32_t>::get_payload_length() {
 template<>
 void IntData<uint32_t>::write_payload(TransmitPacketInterface& packet) {
   packet.write_uint32(*this);
+}
+
+//template<typename T>
+template<>
+size_t IntData<uint8_t>::get_header_kvrs_length() {
+  return Data::get_header_kvrs_length()
+      + 1 + 1;  // data length
+}
+
+//template<typename T>
+template<>
+void IntData<uint8_t>::write_header_kvrs(TransmitPacketInterface& packet) {
+  Data::write_header_kvrs(packet);
+  packet.write_uint8(RECORDID_INT_LENGTH);
+  packet.write_uint8(get_payload_length());
 }
 
 }
