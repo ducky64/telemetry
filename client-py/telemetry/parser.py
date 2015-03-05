13450fafbcac51072229eaae78e6ccbba6b7a0d5
ducky64 <richard.lin@berkeley.edu>
2015-03-04 19:42:40 -0800

    Better parser architecture

--- client-py/telemetry/parser.py
+++ client-py/telemetry/parser.py
@@ -2,16 +2,14 @@ from collections import namedtuple, deque
 import time
 
 import serial
+from test.test_dis import outer
 
-TelemetryPacket = namedtuple('TelemetryPacket', ['opcode', 'sequence', 'payload'])
+# lifted from https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
 
-KvRecord = namedtuple('KvRecord', ['record_id', 'value'])
-DataHeader = namedtuple('DataHeader', ['data_id', 'data_type', 'kv_records'])
-HeaderPacket = namedtuple('DataDefinition', ['data_headers'])
-
-DataRecord = namedtuple('DataRecord', ['data_id', 'value'])
-DataPacket = namedtuple('Data', ['data_records'])
-
+# Global constants defined by the telemetry protocol.
 # TODO: parse constants from cpp header
 SOF_BYTE = [0x05, 0x39]
 
@@ -23,138 +21,218 @@ DATAID_TERMINATOR = 0x00
 DATATYPE_NUMERIC = 0x01
 DATATYPE_NUMERIC_ARRAY = 0x02
 
+NUMERIC_SUBTYPE_UINT = 0x01
+NUMERIC_SUBTYPE_SINT = 0x02
+NUMERIC_SUBTYPE_FLOAT = 0x03
+
 RECORDID_TERMINATOR = 0x00
-RECORDID_INTERNAL_NAME = 0x01
-RECORDID_DISPLAY_NAME = 0x02
-RECORDID_UNITS = 0x03
 
-RECORDID_OVERRIDE_CTL = 0x08
-RECORDID_OVERRIDE_DATA = 0x08
+# Deserialization functions that (destructively) reads data from the input "stream".
+def deserialize_uint8(byte_stream):
+# TODO: handle overflow
+  return byte_stream.popleft()
 
-RECORDID_NUMERIC_SUBTYPE = 0x40
-RECORDID_NUMERIC_LENGTH = 0x41
+def deserialize_bool(byte_stream):
+# TODO: handle overflow
+  return not(byte_stream.popleft() == 0)
 
-RECORDID_NUMERIC_ARRAY_SUBTYPE = 0x40
-RECORDID_NUMERIC_ARRAY_LENGTH = 0x41
-RECORDID_NUMERIC_ARRAY_COUNT = 0x42
+def deserialize_uint16(byte_stream):
+  # TODO: handle overflow
+  return byte_stream.popleft() << 8 | byte_stream.popleft()
 
-RECORDID_GLOBAL_END = 0x40
+def deserialize_uint32(byte_stream):
+  # TODO: handle overflow
+  return (byte_stream.popleft() << 24 
+         | byte_stream.popleft() << 16
+         | byte_stream.popleft() << 8
+         | byte_stream.popleft())
 
-PACKET_LENGTH_BYTES = 2
+def deserialize_string(byte_stream):
+  # TODO: handle overflow
+  outstr = ""
+  data = byte_stream.popleft()
+  while data:
+    outstr += chr(data)
+    data = byte_stream.popleft()
+  return outstr
 
-# lifted from https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
-def enum(*sequential, **named):
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    return type('Enum', (), enums)
+PACKET_LENGTH_BYTES = 2 # number of bytes in the packet length field
+
+class TelemetryDeserializationError(Exception):
+  pass
+
+class NoRecordIdError(TelemetryDeserializationError):
+  pass
+class MissingKvrError(TelemetryDeserializationError):
+  pass
+
+datatype_registry = {}
+class TelemetryData:
+  def __repr__(self):
+    out = self.__class__.__name__
+    for _, record_desc in self.get_kvrs_dict().items():
+      record_name, _ = record_desc
+      out += " %s=%s" % (record_name, repr(getattr(self, record_name)))
+    return out
   
-DecoderState = enum('SOF', 'LENGTH', 'DATA')
-
-class TelemetryPacketDecoder():
-  def __init__(self, bytes):
-    self.bytes = bytes
-
-  def read_uint8(self):
-    # TODO: handle overflow
-    return self.bytes.popleft()
+  @classmethod
+  def get_kvrs_dict(cls):
+    """Returns a dict of record id => (record name, deserialization function) known by this class.
+    The record name is used as an instance variable name if the KVR is read in.
+    """
+    return {
+      0x01: ('internal_name', deserialize_string),
+      0x02: ('display_name', deserialize_string),
+      0x03: ('units', deserialize_string),
+      # TODO: make more robust by allowing defaults / partial parses
+      # add record 0x08 = freeze
+    }
   
-  def read_uint16(self):
-    # TODO: handle overflow
-    return self.bytes.popleft() << 8 | self.bytes.popleft()
-
-  def read_uint32(self):
-    # TODO: handle overflow
-    return (self.bytes.popleft() << 24 
-           | self.bytes.popleft() << 16
-           | self.bytes.popleft() << 8
-           | self.bytes.popleft())
+  @staticmethod
+  def decode_header(data_id, byte_stream):
+    """Decodes a data header from the telemetry stream, automatically detecting and returning
+    the correct TelemetryData subclass object.
+    """
+    opcode = byte_stream[0]
+    if opcode not in datatype_registry:
+      raise NoOpcodeError("No opcode %02x" % opcode)
+    data_cls = datatype_registry[opcode]
+    return data_cls(data_id, byte_stream)
   
-  def read_string(self):
-    # TODO: handle overflow
-    outstr = ""
-    data = self.bytes.popleft()
-    while data:
-      outstr += chr(data)
-      data = self.bytes.popleft()
-    return outstr
-  
-  def decode(self):
-    opcode = self.read_uint8()
-    sequence = self.read_uint8()
+  def __init__(self, data_id, byte_stream):
+    self.data_id = data_id
+    self.data_type = deserialize_uint8(byte_stream)
     
-    if opcode == OPCODE_HEADER:
-      payload = self.decode_header_packet()
-#     elif opcode == OPCODE_DATA:
-#       payload = self.decode_data_packet()
+    self.internal_name = "%02x" % data_id
+    self.display_name = self.internal_name
+    self.units = ""
+    
+    self.decode_kvrs(byte_stream)
+    
+  def decode_kvrs(self, byte_stream):
+    """Destructively reads in a sequence of KVRs from the input stream, writing
+    the known ones as instance variables and throwing exceptions on unknowns.
+    """
+    kvrs_dict = self.get_kvrs_dict()
+    while True:
+      record_id = deserialize_uint8(byte_stream)
+      if record_id == RECORDID_TERMINATOR:
+        break
+      elif record_id not in kvrs_dict:
+        raise NoRecordIdError("No RecordId %02x in %s" % (record_id, self.__class__.__name__))
+      record_name, record_deserializer = kvrs_dict[record_id]
+      setattr(self, record_name, record_deserializer(byte_stream))
+      
+    # check that all KVRs have been read in / defaulted
+    for record_id, record_desc in kvrs_dict.items():
+      record_name, _ = record_desc
+      if not hasattr(self, record_name):
+        raise NoRecordIdError("%s missing record %02x: %s" % (self.__class__.__name__, record_id, record_name))
+
+  def deserialize_data(self, byte_stream):
+    """Destructively reads in the data of this type from the input stream.
+    """
+    raise NotImplementedError
+  
+  
+  
+class UnknownNumericSubtype(Exception):
+  pass
+  
+class NumericData(TelemetryData):
+  @classmethod
+  def get_kvrs_dict(cls):
+    newdict = super(NumericData, cls).get_kvrs_dict().copy()
+    newdict.update({ 
+      0x40: ('subtype', deserialize_uint8),
+      0x41: ('length', deserialize_uint8),
+    })
+    return newdict
+    
+  def deserialize_data(self, byte_stream):
+    if self.subtype == NUMERIC_SUBTYPE_UINT:
+      value = 0
+      remaining = self.length
+      while remaining > 0:
+        value = value << 8 | deserialize_uint8(byte_stream)
+        remaining -= 1
+      return value
+      # TODO: add support for sint / floats
     else:
-      payload = self.bytes
-    
-    return TelemetryPacket(opcode, sequence, payload)
-    
-  def decode_header_packet(self):
-    data_headers = {}
-    next_data_header = self.decode_data_header()
-    while next_data_header != DATAID_TERMINATOR:
-      data_headers[next_data_header.data_id] = next_data_header
-      next_data_header = self.decode_data_header()      
-    return HeaderPacket(data_headers) 
+      raise UnknownNumericSubtype("Unknown subtype %02x" % self.subtype)
+  
+datatype_registry[DATATYPE_NUMERIC] = NumericData
 
-  def decode_data_header(self):
-    kvrs = {}
-    data_id = self.read_uint8()
-    if data_id == DATAID_TERMINATOR:
-      return DATAID_TERMINATOR
-    data_type = self.read_uint8()
-    next_kvr = self.decode_kvr(data_type)
-    while next_kvr != RECORDID_TERMINATOR:
-      kvrs[next_kvr.record_id] = next_kvr
-      next_kvr = self.decode_kvr(data_type)
-    return DataHeader(data_id, data_type, kvrs)
+class NumericArrayData(TelemetryData):
+  @classmethod
+  def get_kvrs_dict(cls):
+    newdict = super(NumericData, cls).get_kvrs_dict().copy()
+    newdict.update({ 
+      0x40: ('subtype', deserialize_uint8),
+      0x41: ('length', deserialize_uint8),
+      0x42: ('count', deserialize_uint8),
+    })
+    return newdict 
+datatype_registry[DATATYPE_NUMERIC_ARRAY] = NumericArrayData
 
-  def decode_kvr(self, data_type):
-    record_id = self.read_uint8()
-    if record_id == RECORDID_TERMINATOR:
-      return RECORDID_TERMINATOR
+
+
+class NoOpcodeError(TelemetryDeserializationError):
+  pass
+class DuplicateDataIdError(TelemetryDeserializationError):
+  pass
+
+opcodes_registry = {}
+class TelemetryPacket:
+  @staticmethod
+  def decode(byte_stream):
+    opcode = byte_stream[0]
+    if opcode not in opcodes_registry:
+      raise NoOpcodeError("No opcode %02x" % opcode)
+    packet_cls = opcodes_registry[opcode]
+    return packet_cls(byte_stream)
+  
+  def __init__(self, byte_stream):
+    self.opcode = deserialize_uint8(byte_stream)
+    self.sequence = deserialize_uint8(byte_stream)
+    self.decode_payload(byte_stream)
     
-    if record_id < RECORDID_GLOBAL_END:
-      if record_id == RECORDID_INTERNAL_NAME:
-        return KvRecord('internal_name', self.read_string())
-      elif record_id == RECORDID_DISPLAY_NAME:
-        return KvRecord('display_name', self.read_string())
-      elif record_id == RECORDID_UNITS:
-        return KvRecord('units', self.read_string())
-      else:
-        raise RuntimeError("Unknown record ID %i for datatype %i" 
-                           % (record_id, data_type))
-    elif data_type == DATATYPE_NUMERIC:
-      if (record_id == RECORDID_NUMERIC_SUBTYPE):
-        return KvRecord('subtype', self.read_uint8())
-      elif (record_id == RECORDID_NUMERIC_LENGTH):
-        return KvRecord('length', self.read_uint8())
-      else:
-        raise RuntimeError("Unknown record ID %i for datatype %i" 
-                           % (record_id, data_type))
-    elif data_type == DATATYPE_NUMERIC_ARRAY:
-      if (record_id == RECORDID_NUMERIC_ARRAY_SUBTYPE):
-        return KvRecord('subtype', self.read_uint8())
-      elif (record_id == RECORDID_NUMERIC_ARRAY_LENGTH):
-        return KvRecord('length', self.read_uint8())
-      elif (record_id == RECORDID_NUMERIC_ARRAY_COUNT):
-        return KvRecord('count', self.read_uint8())
-      else:
-        raise RuntimeError("Unknown record ID %i for datatype %i" 
-                           % (record_id, data_type))
-        
-  def decode_data_packet(self):
-    pass
+  def decode_payload(self, byte_stream):
+    raise NotImplementedError
+
+class HeaderPacket(TelemetryPacket):
+  def __repr__(self):
+    return "Header [%i]: %s" % (self.sequence, repr(self.data))
+    
+  def decode_payload(self, byte_stream):
+    self.data = {}
+    while True:
+      data_id = deserialize_uint8(byte_stream)
+      if data_id == DATAID_TERMINATOR:
+        break
+      elif data_id in self.data:
+        raise DuplicateDataIdError("Duplicate DataId %02x" % data_id)
+      self.data[data_id] = TelemetryData.decode_header(data_id, byte_stream)
+      
+opcodes_registry[OPCODE_HEADER] = HeaderPacket
+
+class DataPacket(TelemetryPacket):
+  pass
+
+opcodes_registry[OPCODE_DATA] = HeaderPacket  
+
+
 
 class TelemetrySerial:
+  DecoderState = enum('SOF', 'LENGTH', 'DATA')
+  
   def __init__(self, serial):
     self.serial = serial
     
     self.rx_packets = deque()  # queued decoded packets
     
     # decoder state machine variables
-    self.decoder_state = DecoderState.SOF;  # expected next byte
+    self.decoder_state = self.DecoderState.SOF;  # expected next byte
     self.decoder_pos = 0; # position within decoder_State
     self.packet_length = 0;  # expected packet length
     self.packet_buffer = deque()
@@ -164,7 +242,7 @@ class TelemetrySerial:
   def process_rx(self):
     while self.serial.inWaiting():
       rx_byte = ord(self.serial.read())
-      if self.decoder_state == DecoderState.SOF:
+      if self.decoder_state == self.DecoderState.SOF:
         self.packet_buffer.append(rx_byte)
         
         if rx_byte == SOF_BYTE[self.decoder_pos]:
@@ -172,32 +250,33 @@ class TelemetrySerial:
           if self.decoder_pos == len(SOF_BYTE):
             self.packet_length = 0
             self.decoder_pos = 0
-            self.decoder_state = DecoderState.LENGTH
+            self.decoder_state = self.DecoderState.LENGTH
         else:
           self.data_buffer.extend(self.packet_buffer)
           for byte in self.packet_buffer:
             print(chr(byte), end="")
           self.packet_buffer = deque()
           self.decoder_pos = 0
-      elif self.decoder_state == DecoderState.LENGTH:
+      elif self.decoder_state == self.DecoderState.LENGTH:
         self.packet_length = self.packet_length << 8 | rx_byte
         self.decoder_pos += 1
         if self.decoder_pos == PACKET_LENGTH_BYTES:
           self.packet_buffer = deque()
           self.decoder_pos = 0
-          self.decoder_state = DecoderState.DATA
-      elif self.decoder_state == DecoderState.DATA:
+          self.decoder_state = self.DecoderState.DATA
+      elif self.decoder_state == self.DecoderState.DATA:
         self.packet_buffer.append(rx_byte)
         self.decoder_pos += 1
         if self.decoder_pos == self.packet_length:
-          decoded = TelemetryPacketDecoder(self.packet_buffer).decode()
+          decoded = TelemetryPacket.decode(self.packet_buffer)
           self.rx_packets.append(decoded)
           print("")
           print(decoded)
           self.packet_buffer = deque()
           
           self.decoder_pos = 0
-          self.decoder_state = DecoderState.SOF
+          self.decoder_state = self.DecoderState.SOF
+          # TODO: add byte destuffing
       else:
         raise RuntimeError("Unknown DecoderState")    
 
