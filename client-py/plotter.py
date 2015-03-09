42e4fb205758b82082f58a643e753e634d933f85
ducky64 <richard.lin@berkeley.edu>
2015-03-09 16:25:23 -0700

    ... and fix the comparison on the last commit

--- client-py/plotter.py
+++ client-py/plotter.py
@@ -136,7 +136,7 @@ class WaterfallPlot(BasePlot):
         
       indep_cutoff = indep_val - self.indep_span
       
-      while self.x_mesh[0][0] < indep_cutoff or self.x_mesh[0][0] < indep_val:
+      while self.x_mesh[0][0] < indep_cutoff or self.x_mesh[0][0] > indep_val:
         self.x_mesh = np.delete(self.x_mesh, (0), axis=0)
         self.y_mesh = np.delete(self.y_mesh, (0), axis=0)
         self.data_array = np.delete(self.data_array, (0), axis=0)
